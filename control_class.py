f61d652ee8d4181ea81184fdacc3f98cf561112c
Teccas17 <teccaswire@gmail.com>
2025-08-05 00:42:39 -0400

    Update control_class.py

--- control_class.py
+++ control_class.py
@@ -1,95 +1,80 @@
-from mouse_driver.InputMethods.InputMethodGFCK import InputMethodGFCK
-from threading import Thread
-from win32api import GetKeyState
-from time import sleep
+import time
+import serial
+import win32api
+import win32con
 
 
-class Control(Thread):
-    name = 'Control'  # used for logging
-    MouseInput=InputMethodGFCK()
-    def __init__(self):
-        super(Control, self).__init__()
+# ---- Makcu Interface ----
+class MakcuInterface:
+    def __init__(self, com_port='COM3', baudrate=4000000):
+        try:
+            self.serial = serial.Serial(port=com_port, baudrate=baudrate, timeout=0)
+            time.sleep(2)  # allow time for Makcu to initialize
+            print(f"[Makcu] Connected on {com_port}")
+        except Exception as e:
+            print(f"[Makcu] Failed to connect: {e}")
+            self.serial = None
 
-        self.stop = False
-        self.running = False
-        self.active = False
-        self.move_x = 0
-        self.move_y = 0
-        self.timing = 0
-        self.r_mouse = 0x02
-        self.l_mouse = 0x01
-        self.move_click = True
+    def send_command(self, cmd: str):
+        if self.serial and self.serial.is_open:
+            self.serial.write((cmd + '\n').encode('utf-8'))
 
-    def run(self, name=name):
-        """The main loop"""
-        self.running = True  #
-        while self.running:
-            self.check_status()
-            if self.active:
-                if not self.move_click:
-                    self.movement()
-                    continue
-                self.movement_with_click()
-                continue
+    def move(self, dx: int, dy: int):
+        self.send_command(f"MOVE {dx} {dy}")
 
-            sleep(0.1)
+    def click(self, button='left'):
+        if button == 'left':
+            self.send_command("CLICK LEFT")
+        elif button == 'right':
+            self.send_command("CLICK RIGHT")
 
-    def check_status(self):
-        """Checks if both mouse buttons are pressed"""
-        if GetKeyState(self.r_mouse) < 0 and GetKeyState(self.l_mouse) < 0:
-            self.active = True
+    def release(self, button='left'):
+        if button == 'left':
+            self.send_command("RELEASE LEFT")
+        elif button == 'right':
+            self.send_command("RELEASE RIGHT")
+
+
+# ---- Control Class ----
+class Control:
+    def __init__(self, use_makcu=False, makcu_port='COM3'):
+        self.use_makcu = use_makcu
+        self.makcu = None
+        if self.use_makcu:
+            self.makcu = MakcuInterface(com_port=makcu_port)
+
+    def move_mouse(self, dx, dy):
+        if self.use_makcu and self.makcu:
+            self.makcu.move(dx, dy)
         else:
-            self.active = False
+            win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, dx, dy, 0, 0)
 
-    def cleanup(self):
-        """call this to stop main Loop"""
-        self.running = False
-        self.join()
+    def click_mouse(self, button='left'):
+        if self.use_makcu and self.makcu:
+            self.makcu.click(button)
+        else:
+            if button == 'left':
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            elif button == 'right':
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
 
-    def reset(self):
-        """"Stopping to update Values (x,y,t)"""
-        self.stop = True
-        self.move_x, self.move_y, self.timing, self.move_click = 0, 0, 0, False
+    def press_mouse(self, button='left'):
+        if self.use_makcu and self.makcu:
+            self.makcu.click(button)
+        else:
+            if button == 'left':
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+            elif button == 'right':
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
 
-    def update(self, x, y, t, move_click=False):
-        """ Update Recoil Values
-            input (x,y,t)
-        """
-        self.reset()
-        self.move_x, self.move_y, self.timing,self.move_click = x, y, (t * 0.001) , move_click
-        self.stop = False
+    def release_mouse(self, button='left'):
+        if self.use_makcu and self.makcu:
+            self.makcu.release(button)
+        else:
+            if button == 'left':
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            elif button == 'right':
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
 
-    def current(self, debug=False):
-        """prints current Value for debug / logging
-        input printout True to print
-        """
-        if debug:
-            print(f'current values: {self.move_x, self.move_y, self.timing, self.move_click}')
-        return self.move_x, self.move_y, self.timing, self.move_click
-
-    def movement(self):
-        if not self.stop:
-            self.MouseInput.moveRelative(self.move_x, self.move_y) # move the mouse
-            sleep(self.timing)  # sleep for t milliseconds
-
-    def movement_with_click(self):
-        if not self.stop:
-            self.MouseInput.up(1)
-            self.MouseInput.moveRelative(self.move_x, self.move_y)  # move the mouse
-            sleep(self.timing)  # sleep for t milliseconds
-            self.MouseInput.down(1)
-
-if __name__ == "__main__":
-    import keyboard
-    con = Control()
-    con.start()
-    con.update(0, 5, 6)
-    con.current(debug=True)
-    def stop():
-        con.cleanup()
-    def update():
-        con.update(0, 3, 8)
-        con.current(debug=True)
-    keyboard.add_hotkey("F10", stop, suppress=True)
-    keyboard.add_hotkey("F9", update, suppress=True)
-    con.run(name=Control)
